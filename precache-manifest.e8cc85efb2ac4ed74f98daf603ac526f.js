8f1088580e431e8ac2a578b74c2ff72a533847ac
Gabriel Sandu <allmediacreation@gmail.com>
2019-10-21 09:47:28 +0300

    Updates

---
+++ precache-manifest.e8cc85efb2ac4ed74f98daf603ac526f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467a90534e5f397b1f9dd8a8d3bea353",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "04082a2325030e5d6b96",
+    "url": "/my-app/static/css/main.3ae534fe.chunk.css"
+  },
+  {
+    "revision": "53057dfaadcf7c8519e8",
+    "url": "/my-app/static/js/2.deb1b1ea.chunk.js"
+  },
+  {
+    "revision": "04082a2325030e5d6b96",
+    "url": "/my-app/static/js/main.6fa1a7a2.chunk.js"
+  },
+  {
+    "revision": "b4472f7c1cee8e35d436",
+    "url": "/my-app/static/js/runtime-main.48de0db9.js"
+  }
+]);
